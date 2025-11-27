dec05eb5b69a24025ee93390b8e62735ad58634b
mantas <mantasvisakavicius@gmail.com>
2025-11-27 17:24:23 +0200

    Update db_setup.sh
    PAAISKINIMAS KAIP VEIKIA

--- db_setup.sh
+++ db_setup.sh
@@ -3,24 +3,26 @@ a
 mkdir -p /home/mavi1016/.ansible/dbstack
 cd /home/mavi1016/.ansible/dbstack
 
+#PRADEKIT NUO my.cnf ir init.sql POTO I DOCKERFILE ZIUREKIT
+
 cat > Dockerfile <<"DOC"
 # dbstack/Dockerfile – MariaDB for HOSPITAL
 FROM mariadb:10.11
-COPY my.cnf /etc/mysql/my.cnf
-COPY init.sql /docker-entrypoint-initdb.d/
+COPY my.cnf /etc/mysql/my.cnf    musu my.cnf (KONFIGURACIJOS FAILUS KURIS NUSTATO KAIP MARIADB VEIKS)
+COPY init.sql /docker-entrypoint-initdb.d/   (tiesiog instrukcijos MariaDB, ką sukurti startuojant konteinerį.)
 EXPOSE 3306
 # Default ENTRYPOINT from image starts mysqld
 DOC
 
 cat > my.cnf <<"DAT"
-[mysqld]
-user=mysql
-bind-address=0.0.0.0
-skip-name-resolve
-port=3306
-socket=/run/mysqld/mysqld.sock
-datadir=/var/lib/mysql
-skip-networking=0
+[mysqld]          #Tells MariaDB that the lines underneath apply to the server daemon (mysqld).                        #KITAIP SAKANT my.cnf YRA KONFIGURACIJOS FAILAS!!!!
+user=mysql         #MariaDB daemon will run as the mysql system user inside the container.
+bind-address=0.0.0.0         #Accept connections from any network interface.
+skip-name-resolve         #MariaDB does NOT try to resolve hostnames → it uses ONLY IP addresses.
+port=3306             #Listen for TCP connections on port 3306.
+socket=/run/mysqld/mysqld.sock         #It's just the local communication channel.
+datadir=/var/lib/mysql   #“Store all database files (tables, indexes, logs) in /var/lib/mysql.”
+skip-networking=0     #Enable TCP networking so containers and hosts can connect.
 DAT
 
 cat > init.sql <<"SQL"
@@ -29,12 +31,8 @@ cat > init.sql <<"SQL"
 CREATE DATABASE IF NOT EXISTS hospital;
 
 -- main db user used by PHP in containers
-CREATE USER IF NOT EXISTS 'hospital_user'@'%' IDENTIFIED BY 'hospital_pass';
-GRANT ALL PRIVILEGES ON hospital.* TO 'hospital_user'@'%';
-
--- optional host-specific user (if they ever use hostname instead of IP)
-CREATE USER IF NOT EXISTS 'hospital_user'@'arnas-webserver-vm-arba1037.cloud' IDENTIFIED BY 'hospital_pass';
-GRANT ALL PRIVILEGES ON hospital.* TO 'hospital_user'@'arnas-webserver-vm-arba1037.cloud';
+CREATE USER IF NOT EXISTS 'hospital_user'@'%' IDENTIFIED BY 'hospital_pass';        #MariaDB vartotojas, kurį PHP naudos prisijungti prie DB
+GRANT ALL PRIVILEGES ON hospital.* TO 'hospital_user'@'%'
 
 FLUSH PRIVILEGES;
 
@@ -78,21 +76,21 @@ SQL
 
 cat > docker-compose.yml <<"COM"
 version: "3.9"
-services:
+services:    #kokius konteinerius paleisti? 
   mariadb:
-    build: .
+    build: .    #kur statyti dockerfile? sitoje direktorijoje kurioje esame.
     container_name: hospital_db
     environment:
-      MYSQL_ROOT_PASSWORD: rootpassword
-      MYSQL_DATABASE: hospital
+      MYSQL_ROOT_PASSWORD: rootpassword            #VISI SITIE DUOMENYS BUS NAUDOJAMI CONFIG.PHP TENAIS MES SUKURSIM IS SITU mysql_root_pass ,database,user ir password VARIABLE KURIS TAPS prisijungimo prie DB objektu, kurį gali naudoti visur PHP kode: skaityti, rašyti, registruoti pacientus, gydytojus, appointments, ir t.t.
+      MYSQL_DATABASE: hospital                        #Viskas veikia kaip tiltas tarp PHP aplikacijos ir MariaDB konteinerio.
       MYSQL_USER: hospital_user
       MYSQL_PASSWORD: hospital_pass
     ports:
-      - "3306:3306"
+      - "3306:3306"                #leidzia belekam jungtis and sito porto
     volumes:
-      - db_data:/var/lib/mysql
+      - db_data:/var/lib/mysql        #issaugo visus init.sql failus i db_data
 volumes:
-  db_data:
+  db_data:    #laiko visus duomenis cia jeigu conteineris issijungtu kad jie neissitrintu
 COM
 
 COM
