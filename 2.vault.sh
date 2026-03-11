63972c765db1a7d0df9e085bdd6bd0487fcd1201
mantas <mantasvisakavicius@gmail.com>
2026-03-11 18:07:29 +0200

    Update Ansible passwords in vault.yml

--- 2.vault.sh
+++ 2.vault.sh
@@ -3,13 +3,13 @@
 cd /home/mavi1016/.ansible
 cat > vault.yml << "safe"
 ansibleuser1: "naka1314"
-ansiblepass1: "sataNZetro"
+ansiblepass1: "asdasfagg"
 ansibleuser2: "dope1157"
 ansiblepass2: "h7j9gq2DWAHK"
 ansibleuser3: "viba1062"
-ansiblepass3: "viba1062"
+ansiblepass3: "gagasasdasd"
 ansibleuser4: "arba1037"
-ansiblepass4: "arnas2005"
+ansiblepass4: "klaksdlasko"
 safe
 
 echo "domantas" > vault.txt
